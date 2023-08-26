3ff3d7aa315db9a6b7eb1d89b48a7f004a957760
ROMANVIKI <vikiadhi2016@gmail.com>
2023-08-26 19:57:01 +0530

    aug-26

--- django-server/myapp/views.py
+++ django-server/myapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import openai
 # Create your views here.
 def api(request):
-    return HttpResponse("Hello from Django")
+    response = openai.Image.create(
+        prompt="a white siamese cat",
+        n=1,
+        size="256x256",
+        api_key='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',
+        
+    )
+    image_url = response['data'][0]['url']
+    return image_url
